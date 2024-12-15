fabeb488d16c53cedfe8e1082a34a1d5d6d67502
Maruf Ahmed <marufahmed.m101@gmail.com>
2024-12-15 01:52:00 +0000

    feat: added bufferline and neotree keymaps

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,59 @@
+-- return {
+--   'akinsho/bufferline.nvim',
+--   version = "*",
+--   dependencies = 'nvim-tree/nvim-web-devicons',
+--   config = function()
+--     vim.opt.termguicolors = true
+--     require("bufferline").setup{}
+--   end
+-- }
+--
+return {
+  "akinsho/bufferline.nvim",
+  event = "BufWinEnter",
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
+    { "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+    { "[b", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
+    { "]b", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+    { "[B", "<Cmd>BufferLineMovePrev<CR>", desc = "Move Buffer Prev" },
+    { "]B", "<Cmd>BufferLineMoveNext<CR>", desc = "Move Buffer Next" },
+  },
+  opts = {
+    options = {
+      close_command = function(bufnr)
+        require("bufdelete").bufdelete(bufnr, true)
+      end,
+      right_mouse_command = function(bufnr)
+        require("bufdelete").bufdelete(bufnr, true)
+      end,
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = false,
+      diagnostics_indicator = function(_, _, diagnostics)
+        local icons = { Error = "", Warn = "", Info = "", Hint = "" }
+        return (diagnostics.error and icons.Error .. diagnostics.error .. " " or "")
+          .. (diagnostics.warning and icons.Warn .. diagnostics.warning or "")
+      end,
+      offsets = {
+        { filetype = "neo-tree", text = "File Explorer", highlight = "Directory", text_align = "left" },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+
+    -- Auto-update bufferline when buffers are modified
+    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+      callback = function()
+        vim.schedule(function()
+          pcall(require("bufferline").refresh)
+        end)
+      end,
+    })
+  end,
+}
+
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,32 +1,114 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      vim.keymap.set('n', '<leader>e', function()
-        local neotree_open = false
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    -- Keymapping for toggling Neo-tree
+    vim.keymap.set('n', '<leader>e', function()
+      local neotree_open = false
 
-        -- Check if a Neotree window is open
-        for _, win in ipairs(vim.api.nvim_list_wins()) do
-          local buf = vim.api.nvim_win_get_buf(win)
-          local buf_ft = vim.api.nvim_buf_get_option(buf, 'filetype')
-          if buf_ft == 'neo-tree' then
-            neotree_open = true
-            break
-          end
+      -- Check if a Neo-tree window is open
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local buf_ft = vim.api.nvim_buf_get_option(buf, 'filetype')
+        if buf_ft == 'neo-tree' then
+          neotree_open = true
+          break
         end
+      end
 
-        if neotree_open then
-          -- Close Neotree if already open
-          vim.cmd('Neotree close')
-        else
-          -- Open Neotree if not open
-          vim.cmd('Neotree filesystem reveal right')
+      if neotree_open then
+        -- Close Neo-tree if already open
+        vim.cmd('Neotree close')
+      else
+        -- Open Neo-tree if not open
+        vim.cmd('Neotree filesystem reveal right')
+      end
+    end, {})
+
+    -- Configure Neo-tree
+    require("neo-tree").setup({
+      sources = { "filesystem", "buffers", "git_status" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        position = "right", -- Ensure Neo-tree opens on the right
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["<space>"] = "none",
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+          ["O"] = {
+            function(state)
+              local path = state.tree:get_node().path
+              vim.fn.system({ "xdg-open", path }) -- Adjust for macOS or Windows
+            end,
+            desc = "Open with System Application",
+          },
+          ["P"] = { "toggle_preview", config = { use_float = false } },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- If nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        git_status = {
+          symbols = {
+            unstaged = "󰄱",
+            staged = "󰱒",
+          },
+        },
+      },
+      event_handlers = {
+        {
+          event = "file_moved",
+          handler = function(data)
+            print("File moved from", data.source, "to", data.destination)
+          end,
+        },
+        {
+          event = "file_renamed",
+          handler = function(data)
+            print("File renamed from", data.source, "to", data.destination)
+          end,
+        },
+      },
+    })
+
+    -- Auto-close Neo-tree when exiting Neovim
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      callback = function()
+        vim.cmd("Neotree close")
+      end,
+    })
+
+    -- Refresh Neo-tree git_status after lazygit closes
+    vim.api.nvim_create_autocmd("TermClose", {
+      pattern = "*lazygit",
+      callback = function()
+        if package.loaded["neo-tree.sources.git_status"] then
+          require("neo-tree.sources.git_status").refresh()
         end
-      end, {})
-    end
+      end,
+    })
+  end,
 }
+
